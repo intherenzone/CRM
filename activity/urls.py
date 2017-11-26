63ef05d994d47fa281db57f204a2826c679ecabd
intherenzone <maviles@umd.edu>
2017-11-25 19:42:12 -0500

    fixed save and new button

--- activity/urls.py
+++ activity/urls.py
@@ -6,4 +6,5 @@ app_name = 'activity'
 urlpatterns = [
     url(r'^list/$', views.activity_list, name='list'),
     url(r'^create/$', views.add_activity, name='add_activity'),
+    url(r'^create/$', views.add_activity, name='save'),
     ]
