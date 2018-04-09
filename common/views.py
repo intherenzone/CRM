4d14e95d767a4b8eeae344d13b52a3affc7d0abe
Raymond <raymond180@hotmail.com>
2018-04-09 01:46:48 -0400

    New Index Page
    New stuff on index page. Contains contacts queries

--- common/views.py
+++ common/views.py
@@ -9,9 +9,37 @@ from common.models import *
 from django.contrib.auth.models import User
 from django.conf import settings
 
+from contacts.models import Contact
+
 @login_required
 def home(request):
-    return render(request, 'crm/index.html')
+    username = None
+    if request.user.is_authenticated():
+        username = request.user.username
+    this_user_object = User.objects.filter(username=username)[0]
+    teamsObject_this_user_belongs_to = Team.objects.filter(members__username = this_user_object.username)
+    contacts_lists_the_teams_have = []
+    for team in teamsObject_this_user_belongs_to:
+        contacts_the_team_has = Contact.objects.filter(teams__name = team.name)
+        contacts_lists_the_teams_have.append(contacts_the_team_has)
+    my_contactsObject_this_user_is_assigned_to = Contact.objects.filter(assigned_to__username=this_user_object.username)
+
+    teamsObject_list_this_user_belongs_to = Team.objects.filter(members__username = this_user_object.username)
+    teams_and_contacts_lists_the_teams_have = zip(teamsObject_list_this_user_belongs_to, contacts_lists_the_teams_have)
+
+    #contacts_lists_the_teams_have = []
+    #for i in range(len(teamsObject_this_user_belongs_to)):
+    #    contacts_lists_the_teams_have.append([teamsObject_this_user_belongs_to[i]])
+    #    contacts_the_team_has = Contact.objects.filter(teams__name = contacts_lists_the_teams_have[i][0])
+    #    contacts_lists_the_teams_have[i].append([contacts_the_team_has])
+
+    return render(request, 'crm/index.html', {
+    "teamsObject" : teamsObject_this_user_belongs_to,
+    "my_contactsObjects" : my_contactsObject_this_user_is_assigned_to,
+    "contacts_lists_the_teams_have" : contacts_lists_the_teams_have,
+    #teamsObject_this_user_belongs_to : contacts_lists_the_teams_have,
+    "teams_and_contacts_lists_the_teams_have" : teams_and_contacts_lists_the_teams_have,
+    })
 
 
 @csrf_exempt
