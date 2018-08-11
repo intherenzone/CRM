235b2e64cda5c36c4bee120fb37c4be1518c37fa
Renuka Dalal <rmdalal@terpmail.umd.edu>
2018-08-12 02:21:34 +0400

    Implemented filters for the newsfeed, ordered the news by recency and displayed date of the news

--- common/views.py
+++ common/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import logout, authenticate, login
 from common.forms import *
 from common.models import *
+from django.db.models import Q
 
 from django.contrib.auth.models import User
 from django.conf import settings
@@ -36,145 +37,55 @@ def home(request):
     teams_to_contacts = zip(teams, teams_contacts)
 
     #get the news object
-    # newsfeed = News.objects.all()
     newsfeed = News.objects.order_by('-date')[0:10]
-
-    # Get User Name currently logged in
-    username = request.user
 
     # count activity types for graphing
     activity_types = []
     activity_type_counts = []
-    user_activity_type_counts = []
-
     for activity_type in ACTIVITY_TYPE:
-        activity_types.append(activity_type[0])  # get each type
-        activity_type_counts.append(
-            Activity.objects.filter(activity_type=activity_type[0]).count())  # get count of each type
-        user_activity_type_counts.append(Activity.objects.filter(assigned_to=username,
-                                                                 activity_type=activity_type[0]).count())
-
-    #Donut Pie chart Activity Type
-    figa = {
-        "data": [
-            {
-                "values": user_activity_type_counts,
-                "labels": activity_types,
-                "text": user_activity_type_counts,
-                "textposition": "inside",
-                "domain": {"x": [0, .48]},
-                "hoverinfo": "label+percent",
-                "hole": .4,
-                "type": "pie"
-
-            },
-            {
-                "values": activity_type_counts,
-                "labels": activity_types,
-                "text": activity_type_counts,
-                "textposition": "inside",
-                "domain": {"x": [.52, 1]},
-                "hoverinfo": "label+percent",
-                "hole": .4,
-                "type": "pie"
-            }],
-        "layout": {
-            "title": "Activity Type",
-            "annotations": [
-                {
-                    "font": {
-                        "size": 12
-                    },
-                    "showarrow": False,
-                    "text": "You",
-                    "x": 0.22,
-                    "y": 0.5
-                },
-                {
-                    "font": {
-                        "size": 12
-                    },
-                    "showarrow": False,
-                    "text": "Company",
-                    "x": 0.805,
-                    "y": 0.5
-
-                }
-            ]
-        }
-    }
-    activity_type_chart = plot(figa, output_type='div')
+        activity_types.append(activity_type[0]) # get each type
+        activity_type_counts.append(Activity.objects.filter(activity_type=activity_type[0]).count()) # get count of each type
+    # create activity type pie chart
+    activity_type_trace = go.Pie(labels=activity_types, values=activity_type_counts,
+               hoverinfo='label+percent' , textinfo='value',
+               textfont=dict(size=20),
+               )
+    data = go.Data([activity_type_trace])
+    layout = go.Layout(title='Activity Type',paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)')
+    fig = go.Figure(data=data,layout=layout)
+    activity_type_chart = plot(fig, output_type = 'div')
 
     # Count statuses for graphing
     statuses = []
     status_counts = []
-    user_status_counts = []
     for status in LEAD_STATUS:
         statuses.append(status[0])
         status_counts.append(Activity.objects.filter(status=status[0]).count())
-        user_status_counts.append(Activity.objects.filter(assigned_to=username, status=status[0]).count())
     # create status pie chart
-    fig_stat = {
-        "data": [
-            {
-                "values": user_status_counts,
-                "labels": statuses,
-                "text": user_status_counts,
-                "textposition": "inside",
-                "domain": {"x": [0, .48]},
-                "hoverinfo": "label+percent",
-                "hole": .4,
-                "type": "pie"
-            },
-            {
-                "values": status_counts,
-                "labels": statuses,
-                "text": status_counts,
-                "textposition": "inside",
-                "domain": {"x": [.52, 1]},
-                "hoverinfo": "label+percent",
-                "hole": .4,
-                "type": "pie"
-            }],
-        "layout": {
-            "title": "Activity Status",
-            "annotations": [
-                {
-                    "font": {
-                        "size": 12
-                    },
-                    "showarrow": False,
-                    "text": "You",
-                    "x": 0.22,
-                    "y": 0.5
-                },
-                {
-                    "font": {
-                        "size": 12
-                    },
-                    "showarrow": False,
-                    "text": "Company",
-                    "x": 0.805,
-                    "y": 0.5
-                }
-            ]
-        }
-    }
-    status_chart = plot(fig_stat, output_type='div')
+    status_trace = go.Pie(labels=statuses, values=status_counts,
+               hoverinfo='label+percent' , textinfo='value',
+               textfont=dict(size=20),
+               )
+    data = go.Data([status_trace])
+    layout = go.Layout(title='Activity Status',paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)')
+    fig = go.Figure(data=data,layout=layout)
+    status_chart = plot(fig, output_type = 'div')
 
-    # def plot_pie():
-    return render(request, 'crm/index.html',
-                  {
-                      "contacts": contacts,
-                      "organizations": organizations,
-                      "teams_to_contacts": teams_to_contacts,
-                      "activity_type_chart": activity_type_chart,
-                      "status_chart": status_chart,
-                      "username": username,
-                      "newsfeed": newsfeed
-                  })
+    #def plot_pie():
 
-#csrf_exempt
+
+    return render(request , 'crm/index.html',
+        {
+            "contacts" : contacts,
+            "organizations" : organizations,
+            "teams_to_contacts" : teams_to_contacts,
+            "activity_type_chart" : activity_type_chart,
+            "status_chart" : status_chart,
+            "newsfeed":newsfeed,
+    })
+
+
+@csrf_exempt
 def login_crm(request):
     print('login')
     if request.user.is_authenticated:
@@ -208,8 +119,32 @@ def register_page(request):
     return render(request, 'crm/registration.html', variables)
 
 def newsfeed(request):
-    newsfeed = News.objects.all()
-    return render(request, 'crm/newsfeed.html',{
-                "newsfeed":newsfeed
+    newsfeed = News.objects.order_by('-date').all()
+    page = request.POST.get('per_page')
 
-    })
+    username = request.POST.get('username')
+    module = request.POST.get('module')
+    action_type = request.POST.get('action_type')
+    startdate = request.POST.get('startdate')
+    enddate = request.POST.get('enddate')
+
+    if username:
+        newsfeed = newsfeed.filter(actor__username__icontains=username) 
+    if module=='activity':
+        newsfeed = newsfeed.filter(activity__isnull = False)
+    elif module=='contact':
+        newsfeed = newsfeed.filter(contact__isnull=False)
+    elif module=='organization':
+        newsfeed = newsfeed.filter(organization__isnull=False)
+    elif module=='comment':
+        newsfeed = newsfeed.filter(comment__isnull=False)
+    if action_type:
+        newsfeed = newsfeed.filter(type__icontains=action_type)
+    if startdate:
+        newsfeed = newsfeed.filter(date__gte=startdate)
+    if enddate:
+        newsfeed = newsfeed.filter(date__lte=enddate)
+
+    return render(request, 'crm/newsfeed.html', {
+        'newsfeed': newsfeed 
+        })
